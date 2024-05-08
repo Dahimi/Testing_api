4ac79f8352eb0ff0ecd846c41ff44817c7d263f4
Soufiane <soufiane@alpha10x.com>
2024-05-08 08:24:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tester import *
+
+
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Specify the domains you want to allow, or use '*' for open access
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index():
+    """
+    This is the index page of the API
+    """
+    return {"message": "Hello World"}
+
+@app.post("/test/")
+def execute_test(test: Test):
+    """
+    This is the function to execute the test
+    """
+    return test.run_test()
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
---
+++ tester.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+# from langchain_groq import ChatGroq
+
+
+# api_key = "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"
+# chat = ChatGroq(temperature=0, groq_api_key=api_key, model_name="mixtral-8x7b-32768")
+
+print("here we go again")
+class Test(BaseModel):
+    code: str
+    #make it a field with the options (python, java and javascript)
+    language: str = Field(..., description="The language of the code", example="python")
+    test_type: str = Field(..., description="The type of test", example="unit")
+
+    def run_test(self):
+        print(self)
+        return {"code": self.code, "language": self.language, "test_type": self.test_type}
+
+
